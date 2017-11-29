47b24a9e4f40edf5ded92a5f76f4c4740a5d5bf8
Valery Raulet <vraulet@users.noreply.github.com>
2017-11-29 11:27:29 +0000

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,14 @@ app.post('/todos', (req, res) => {
     })
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(e);
+    })
+});
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
