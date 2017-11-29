eec5f263f0dd2f39b12f237523f02ce82f612fd0
Valery Raulet <vraulet@users.noreply.github.com>
2017-11-29 21:28:55 +0000

    added delete

--- server/server.js
+++ server/server.js
@@ -35,18 +35,35 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
     if (!ObjectId.isValid(id)) {
-        res.status(404).send('');
+        return res.status(404).send('');
     }
 
     Todo.findById(id).then((todo) => {
         if (!todo) {
-            res.status(404).send();
+            return res.status(404).send();
         }
         res.send({todo});
     }).catch((err) => {
         res.status(400).send();
     });
 })
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send(todo);
+    }).catch((err) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
